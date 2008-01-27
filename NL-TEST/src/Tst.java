f404c4627d6438988848fddd76f2826893f4199f
soft_cz <soft_cz>
2008-01-27 22:14:45 +0000

    *** empty log message ***

---
+++ NL-TEST/src/Tst.java
@@ -0,0 +1,19 @@
+import java.io.UnsupportedEncodingException;
+
+import com.twmacinta.util.MD5;
+
+
+
+public class Tst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
